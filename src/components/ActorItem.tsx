c988b71228d2042e0c25c29f1f51794624459383
CreativiTICs <Creativiticscolombia@gmail.com>
2021-05-01 10:58:00 -0400

    First version Done!

---
+++ src/components/ActorItem.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { View, Text, Image, StyleSheet } from 'react-native'
+import { Cast } from '../interfaces/creditsInterface'
+
+interface Props {
+    actor: Cast
+}
+
+export const ActorItem = ({actor}: Props) => {
+
+    const uri = `https://image.tmdb.org/t/p/w500${actor.profile_path}`
+
+    return (
+        <View style={styles.container} >
+            {
+                actor.profile_path && (
+                    <Image
+                        source={{uri}}
+                        style={{width: 50, height: 50, borderRadius: 10}}
+                    />
+                )
+            }
+            <View style={styles.actorInfo} >
+                <Text style={{fontSize: 18, fontWeight: 'bold'}} >{actor.name}</Text>
+                <Text style={{fontSize: 16, opacity: 0.8}} >{actor.character}</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        borderRadius: 10,
+        height: 60,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 10,
+        },
+        shadowOpacity: 0.24,
+        shadowRadius: 6,
+
+        elevation: 8,
+        marginLeft: 15,
+        paddingRight: 10,
+    },
+    actorInfo: {
+        marginLeft: 10,
+    }
+});
--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,29 +1,54 @@
 import { StackScreenProps } from '@react-navigation/stack'
 import React from 'react'
-import { View, Image, StyleSheet, Dimensions, ScrollView, Text } from 'react-native';
+import { View, Image, StyleSheet, Dimensions, ScrollView, Text, ActivityIndicator, TouchableOpacity } from 'react-native';
 import { RootStackParams } from '../navigation/Navigation';
+import { useMovieDetails } from '../hooks/useMovieDetails';
+import { MovieDetails } from '../components/MovieDetails';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const screenHeight = Dimensions.get('screen').height;
 
 interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'> {}
 
-export const DetailScreen = ({route}: Props) => {
+export const DetailScreen = ({route, navigation}: Props) => {
 
     const movie = route.params;
     const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
+    const {isLoading, cast, movieFull} = useMovieDetails(movie.id);
+    
+
     return (
         <ScrollView>
             <View style={styles.imageContainer} >
-                <Image
-                    source={{uri}}
-                    style={styles.posterImage}
-                />
+                <View style={styles.imageBorder} >
+                    <Image
+                        source={{uri}}
+                        style={styles.posterImage}
+                    />
+                </View>
             </View>
             <View style={styles.marginContainer} >
                 <Text style={styles.subTitle} >{movie.original_title}</Text>
                 <Text style={styles.title} >{movie.title}</Text>
             </View>
+            {
+                isLoading
+                ? <ActivityIndicator size={35} color='grey' style={{marginTop: 20}} />
+                : <MovieDetails movieFull={movieFull!} cast={cast}/>
+            }
+
+            {/* Botón para cerrar */}
+            <TouchableOpacity 
+                style={styles.backBtn}
+                onPress={()=>navigation.pop()}
+            >
+                <Icon
+                    color="white"
+                    name="arrow-back-outline"
+                    size={80}
+                />
+            </TouchableOpacity>
         </ScrollView>
     )
 }
@@ -32,9 +57,12 @@ const styles = StyleSheet.create({
     imageContainer:{
         width: '100%',
         height: screenHeight * 0.7,
+    },
+    imageBorder:{
+        flex: 1,
+        overflow: 'hidden',
         borderBottomEndRadius: 25,
         borderBottomStartRadius: 25,
-        overflow: 'hidden',
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -60,5 +88,19 @@ const styles = StyleSheet.create({
     subTitle:{
         fontSize: 16,
         opacity: 0.6
+    },
+    backBtn:{
+        position: 'absolute',
+        zIndex: 999,
+        elevation: 9,
+        top: 30,
+        left: 5,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 10,
+        },
+        shadowOpacity: 0.24,
+        shadowRadius: 6,
     }
 });
